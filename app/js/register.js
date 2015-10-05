668eb727f8e9d0d8c7379a6a621ddfdca2f371ec
salman19 <salmaneldash@gmail.com>
2015-10-05 11:03:54 +0200

    edit js

--- app/js/register.js
+++ app/js/register.js
@@ -1,24 +1,24 @@
 function validateForm()
 {
-var a=document.getElementById('email').value;
-//var b=document.forms["reg"]["fname"].value;
-//var c=document.forms["reg"]["lname"].value;
-//var d=document.forms["reg"]["avatar"].value;
-var e=document.getElementById('password').value;
+  var a=document.getElementById('email').value;
+  //var b=document.forms["reg"]["fname"].value;
+  //var c=document.forms["reg"]["lname"].value;
+  //var d=document.forms["reg"]["avatar"].value;
+  var e=document.getElementById('password').value;
 
-if ((a==null || a=="") && (e==null || e==""))
-  {
-  alert("All Field must be filled out");
-  return false;
-  }
-if (a==null || a=="")
-  {
-  alert("Email must be filled out");
-  return false;
-  }
-if (e==null || e=="")
-  {
-  alert("password must be filled out");
-  return false;
-  }
+  if ((a==null || a=="") && (e==null || e==""))
+    {
+      alert("All Field must be filled out");
+      return false;
+      }
+  if (a==null || a=="")
+    {
+      alert("Email must be filled out");
+      return false;
+      }
+  if (e==null || e=="")
+    {
+      alert("password must be filled out");
+      return false;
+      }
 }
--- app/js/signin.js
+++ app/js/signin.js
@@ -1,13 +1,11 @@
-
 function validateForm(theForm)
 {
- var emailVal = theForm.email.value;
- var passVal = theForm.password.value;
-  if (emailVal==null || emailVal.trim()=="") { 
-    alert('Please type your email');
-    theForm.email.focus();
-    return false; // cancel submission
-  }
-  return true; // allow submit
- 
+	 var emailVal = theForm.email.value;
+	 var passVal = theForm.password.value;
+	  if (emailVal==null || emailVal.trim()=="") { 
+	    alert('Please type your email');
+	    theForm.email.focus();
+	    return false; // cancel submission
+	  }
+	  return true; // allow submit	 
 }
